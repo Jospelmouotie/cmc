962fbeeb3140cbfcb2b68b4c59efdbbdb12f1e01
JOSPEL MOUOTIE <JOSPEL MOUOTIE@2NCORPORATE.LOCAL>
2026-01-16 11:02:16 +0100

    Final fix: remove IDE Helper and Debugbar from config

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-#!/bin/bash
-# Re-générer la liste des services (vu qu'on a utilisé --no-scripts)
-php artisan package:discover --ansi
+
+# Supprimer les anciens caches pour forcer Laravel à relire config/app.php
 php artisan config:clear
 php artisan cache:clear
+php artisan view:clear
+php artisan route:clear
+
+# Re-générer la liste des services disponibles
+php artisan package:discover --ansi
 
 echo "Exécution des migrations..."
 php artisan migrate --force
 
-# AJOUT DE CETTE LIGNE :
 echo "Création du compte administrateur..."
 php artisan db:seed --class=AdminUserSeeder --force
 
@@ -17,6 +20,3 @@ php artisan nksoftcare:active-licence-key
 
 echo "Démarrage d'Apache..."
 exec apache2-foreground
-
-#!/bin/bash
-
